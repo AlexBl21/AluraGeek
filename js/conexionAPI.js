2d1d6892df3e5f5531db843dd187ef9fea3d51fb
AlexBl21 <hblancorolon@gmail.com>
2024-07-09 10:27:58 -0500

    first commit

---
+++ js/conexionAPI.js
@@ -0,0 +1,58 @@
+async function listarProductos() {
+    // Establecemos donde va a buscar los productos
+    // al usar fecth, por defecto realiza una petición: GET la cual retorna los elementos de la URL
+    const conexion = await fetch('https://fake-api-alura-geek-nu.vercel.app/productos');
+    const conexionConvertida = conexion.json();
+
+    return conexionConvertida;
+}
+
+async function enviarProducto(nombre, precio, imagen) {
+    const conexion = await fetch('https://fake-api-alura-geek-nu.vercel.app/productos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nombre: nombre,
+            precio: precio,
+            imagen: imagen
+        })
+    })
+    const conexionConvertida = conexion.json();
+    return conexionConvertida;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+async function eliminarProducto(id) {
+    try {
+        await fetch(`https://fake-api-alura-geek-nu.vercel.app/productos/${id}`, {
+            method: 'DELETE'
+        });
+    } catch (error) {
+        console.error('Error al eliminar el producto del servidor:', error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+export const conexionAPI = {
+    listarProductos, enviarProducto, eliminarProducto
+}
